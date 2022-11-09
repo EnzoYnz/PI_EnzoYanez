2208c212ae3963eb9e853f760eff0bcc3c007d45
EnzoYnz <eyanez@sanfrancisco.utn.edu.ar>
2022-11-09 18:18:44 -0300

    Cambio de directorio a FE
    Se modifico la carpeta contenedora del PI

---
+++ PI_EnzoYanez/FrontEnd/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+  
+    AcercaDeComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
